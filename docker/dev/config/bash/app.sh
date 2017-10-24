9b1f942dc3bd5c98d8f3db8a37e1aa5e851a4df9
Navid Ghahremani <ghahramani.navid@gmail.com>
2017-10-24 17:54:23 +0100

    added some configuration changed server port

--- docker/dev/config/bash/app.sh
+++ docker/dev/config/bash/app.sh
@@ -10,6 +10,6 @@ echo "===========================        DB is initialized       ===============
 
 echo "===========================  Waiting till config server comes up  ======================="
 /opt/wait-for-it.sh --host=config-server --port=8080 --strict --timeout=0
-echo "===========================       Config server is up  =================================="
+echo "===========================      Config server is up  ==================================="
 
 ./gradlew
--- docker/dev/config/bash/config.sh
+++ docker/dev/config/bash/config.sh
@@ -6,6 +6,6 @@ whoami
 
 echo "===========================  Waiting till DB initialization  ==========================="
 /opt/wait-for-it.sh --host=db --port=27017 --strict --timeout=0
-echo "===========================        DB is initialized       ============================="
+echo "===========================       DB is initialized        ============================="
 
 ./gradlew
